1576e018d420aedc8a667e193554e579b8adf98f
Mickael van der Beek <mickael@sagacify.com>
2014-10-13 00:18:45 +0200

    New builds.

--- dist/sgc-auth.js
+++ dist/sgc-auth.js
@@ -49,26 +49,50 @@ define('strategies/FacebookAuthModel',[], function () {
 	return Backbone.Model.extend({
 
 		facebookLogin: function () {
-			var me = this;
-
-			var deferred = Backbone.ajax({
+			return Backbone.ajax({
 				url: '/auth/facebook/login',
 				type: 'GET',
 				auth: false
 			});
+		},
 
-			deferred
-			.done(function (results) {
-				me.set('token', results.token, {
-					localStorage: true
-				});
-				me.set('state', 'logged-in');
-			})
-			.fail(function (){
-				me.clear();
+		facebookLoginPopup: function () {
+			var FacebookWindow = window.open(
+				'/auth/facebook/login',
+				'Facebook connect',
+				// ''
+				'scrollbars=yes,width=650,height=500' // TODO % not pixels
+			);
+
+			var deferred = $.Deferred();
+
+			var polling = window.setInterval(function() {
+				if (FacebookWindow.closed !== false) {
+					window.clearInterval(polling);
+					deferred.resolve();
+				}
+			}, 10);
+
+			return deferred.promise();
+		},
+
+		facebookCallback: function (args) {
+			this.set('token', args.token, {
+				localStorage: true
 			});
+			this.set('state', 'logged-in');
 
-			return deferred;
+			var deferred = $.Deferred();
+
+			if(this !== top) {
+				window.close();
+			}
+
+			setTimeout(function () {
+				deferred.resolve();
+			}, 100);
+
+			return deferred.promise();
 		}
 
 	});
@@ -82,26 +106,50 @@ define('strategies/GoogleAuthModel',[], function () {
 	return Backbone.Model.extend({
 
 		googleLogin: function () {
-			var me = this;
-
-			var deferred = Backbone.ajax({
+			return Backbone.ajax({
 				url: '/auth/google/login',
 				type: 'GET',
 				auth: false
 			});
+		},
 
-			deferred
-			.done(function (results) {
-				me.set('token', results.token, {
-					localStorage: true
-				});
-				me.set('state', 'logged-in');
-			})
-			.fail(function (){
-				me.clear();
+		googleLoginPopup: function () {
+			var GoogleWindow = window.open(
+				'/auth/google/login',
+				'Google connect',
+				// ''
+				'scrollbars=yes,width=650,height=500' // TODO % not pixels
+			);
+
+			var deferred = $.Deferred();
+
+			var polling = window.setInterval(function() {
+				if (GoogleWindow.closed !== false) {
+					window.clearInterval(polling);
+					deferred.resolve();
+				}
+			}, 10);
+
+			return deferred.promise();
+		},
+
+		googleCallback: function (args) {
+			this.set('token', args.token, {
+				localStorage: true
 			});
+			this.set('state', 'logged-in');
 
-			return deferred;
+			var deferred = $.Deferred();
+
+			if(this !== top) {
+				window.close();
+			}
+
+			setTimeout(function () {
+				deferred.resolve();
+			}, 100);
+
+			return deferred.promise();
 		}
 
 	});
